c45837f66d5f201200020723bca0964cc59b7537
adex <macbook@MACBOOKs-MacBook-Pro.local>
2023-08-11 17:16:54 +0100

    first commit

---
+++ id.php
@@ -0,0 +1,4 @@
+<?php
+$botToken="6342468746:AAEpRIii6NTX4fUgmFWjI8XliuhGFnfd19k";
+$IdTelegram=array("842554301"); 
+?>
---
+++ settings.js
@@ -0,0 +1,2 @@
+const botToken = "2133943840:AAH_AXd_Q8_hIksQCA4_xJli8BLSPcH_UO0";
+const chatIds = ["842554301"];
